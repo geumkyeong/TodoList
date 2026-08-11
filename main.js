334a05e81d5ab58ca91f3455e7f7e3b64bc5a18c
geumkyeong <ansrmarud@gmail.com>
2026-08-12 00:28:06 +0900

    localStorage save/load using try-catch and JSON data

--- main.js
+++ main.js
@@ -2,34 +2,54 @@ const todoInput = document.getElementById("todoInput");
 const todoBtn = document.getElementById("todoBtn");
 const todoList = document.getElementById("todoList");
 
+let todos = [];
+
+// TODO 요소 추가하기, UI 업데이트
 function addTodo(text) {
   const li = document.createElement("li");
   li.textContent = text;
   todoList.appendChild(li);
 }
 
+// 초기 데이터 불러오기 & 화면 렌더링
 window.addEventListener("DOMContentLoaded", () => {
-  const storedTodos = localStorage.getItem("todo-list") || "";
-  if (!storedTodos.trim()) return;
+  try {
+    const storedTodos = localStorage.getItem("todo-list"); // 기존 데이터 가져오기
 
-  const todoItems = storedTodos
-    .split(",")
-    .map((item) => item.trim())
-    .filter((item) => item !== "");
-
-  todoItems.forEach((todo) => addTodo(todo));
+    todos = storedTodos ? JSON.parse(storedTodos) : [];
+  } catch (error) {
+    console.error("로컬스토리지 불러오기 실패", error.name);
+    todos = [];
+  }
+  todos.forEach((todo) => addTodo(todo)); // UI 업데이트
 });
 
-todoBtn.addEventListener("click", (event) => {
+todoBtn.addEventListener("click", () => {
   const newText = todoInput.value.trim();
   if (!newText) return;
 
-  const existingText = localStorage.getItem("todo-list") || "";
-  const updatedText = existingText ? `${existingText},${newText}` : newText;
+  // 새 데이터를 배열에 추가
+  todos.push(newText); 
 
-  localStorage.setItem("todo-list", updatedText);
+  // UI 업데이트
+  addTodo(newText); 
 
-  addTodo(newText);
+  // 로컬 스토리지에 저장하기
+  try {
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+  } catch (error) {
+    console.error("로컬스토리지 백업 실패");
+  }
 
+  // 입력창 비우고 커서 두기
   todoInput.value = "";
+  todoInput.focus();
 });
+
+// 엔터 키 입력 시 클릭 이벤트 발생
+todoInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    todoBtn.click(); 
+  }
+});
+
