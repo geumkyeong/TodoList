987632ed7d585965cf7c8bf502d9ae8bae035485
geumkyeong <ansrmarud@gmail.com>
2026-08-14 16:15:14 +0900

    todo item 내부 요소(checkbox, delete button) 추가 & 부모 전체 너비만큼 차지, 화면 축소 시 최소 너비 유지하며 남은 공간을 채우기, 찌그러짐 방지

--- main.js
+++ main.js
@@ -5,9 +5,39 @@ const todoList = document.getElementById("todoList");
 let todos = [];
 
 // TODO 요소 추가하기, UI 업데이트
-function addTodo(text) {
+function addTodo(todo) {
   const li = document.createElement("li");
-  li.textContent = text;
+  li.className = "todo-item";
+
+  // 체크박스, 텍스트를 묶는 그룹
+  const leftGroup = document.createElement("div");
+  leftGroup.className = "left-todo-group";
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "todo-checkbox";
+  checkbox.id = `task${todo.id}`;
+
+  const label = document.createElement("label");
+  label.htmlFor = `task${todo.id}`;
+  label.className = "todo-text";
+  label.textContent = todo.text;  // 텍스트 추가
+
+  // 삭제 버튼
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "×";
+  removeBtn.className = "delete-btn";
+
+  removeBtn.addEventListener("click", function () {
+    li.remove();
+  });
+
+  leftGroup.appendChild(checkbox);
+  leftGroup.appendChild(label);
+
+  li.appendChild(leftGroup);
+  li.appendChild(removeBtn);
+
   todoList.appendChild(li);
 }
 
@@ -21,6 +51,7 @@ window.addEventListener("DOMContentLoaded", () => {
     console.error("로컬스토리지 불러오기 실패", error.name);
     todos = [];
   }
+
   todos.forEach((todo) => addTodo(todo)); // UI 업데이트
 });
 
@@ -28,11 +59,19 @@ todoBtn.addEventListener("click", () => {
   const newText = todoInput.value.trim();
   if (!newText) return;
 
+  const maxId = todos.length > 0 ? Math.max(...todos.map((t) => t.id)) : 0; // 배열에서 추출한 id 값 중 가장 큰 숫자를 반환함
+
+  const newTodo = {
+    id: maxId + 1, // 다음 인덱스
+    text: newText,
+    done: false,
+  };
+
   // 새 데이터를 배열에 추가
-  todos.push(newText); 
+  todos.push(newTodo);
 
   // UI 업데이트
-  addTodo(newText); 
+  addTodo(newTodo);
 
   // 로컬 스토리지에 저장하기
   try {
@@ -49,7 +88,6 @@ todoBtn.addEventListener("click", () => {
 // 엔터 키 입력 시 클릭 이벤트 발생
 todoInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    todoBtn.click(); 
+    todoBtn.click();
   }
 });
-
