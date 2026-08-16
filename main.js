cbbf7803e454b78a8724e0be83021fc1b1be36d3
geumkyeong <ansrmarud@gmail.com>
2026-08-16 22:15:30 +0900

    로컬스토리지에 체크 상태 업데이트 & 체크 여부 반영하여 UI 다시 그리기

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const todoInput = document.getElementById("todoInput");
 const todoBtn = document.getElementById("todoBtn");
 const todoList = document.getElementById("todoList");
 
+const checkbox = document.querySelector(".todo-checkbox");
+
 let todos = [];
 
-// TODO 요소 추가하기, UI 업데이트
+// 하나의 TODO 요소 추가하기
 function addTodo(todo) {
   const li = document.createElement("li");
   li.className = "todo-item";
@@ -16,12 +18,13 @@ function addTodo(todo) {
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.className = "todo-checkbox";
+  checkbox.checked = todo.done;
   checkbox.id = `task${todo.id}`;
 
   const label = document.createElement("label");
   label.htmlFor = `task${todo.id}`;
   label.className = "todo-text";
-  label.textContent = todo.text;  // 텍스트 추가
+  label.textContent = todo.text; // 텍스트 추가
 
   // 삭제 버튼
   const removeBtn = document.createElement("button");
@@ -41,26 +44,42 @@ function addTodo(todo) {
   todoList.appendChild(li);
 }
 
+// UI 업데이트
+function renderTodos(todos) {
+  todoList.innerHTML = "";  // UI 초기화
+
+  todos.forEach((todo) => addTodo(todo));
+}
+
+// 로컬 스토리지에 저장하기
+function saveTodos(todos) {
+  try {
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+  } catch (error) {
+    console.error("로컬스토리지 백업 실패");
+  }
+}
+
 // 초기 데이터 불러오기 & 화면 렌더링
 window.addEventListener("DOMContentLoaded", () => {
   try {
     const storedTodos = localStorage.getItem("todo-list"); // 기존 데이터 가져오기
 
     todos = storedTodos ? JSON.parse(storedTodos) : [];
+
+    renderTodos(todos);
   } catch (error) {
     console.error("로컬스토리지 불러오기 실패", error.name);
     todos = [];
   }
-
-  todos.forEach((todo) => addTodo(todo)); // UI 업데이트
 });
 
 todoBtn.addEventListener("click", () => {
   const newText = todoInput.value.trim();
   if (!newText) return;
 
-  const maxId = todos.length > 0 ? Math.max(...todos.map((t) => t.id)) : 0; // 배열에서 추출한 id 값 중 가장 큰 숫자를 반환함
-
+  // 맨 끝 인덱스
+  const maxId = todos.length > 0 ? Math.max(...todos.map((t) => t.id)) : 0;
   const newTodo = {
     id: maxId + 1, // 다음 인덱스
     text: newText,
@@ -74,18 +93,37 @@ todoBtn.addEventListener("click", () => {
   addTodo(newTodo);
 
   // 로컬 스토리지에 저장하기
-  try {
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-  } catch (error) {
-    console.error("로컬스토리지 백업 실패");
-  }
+  saveTodos(todos);
 
   // 입력창 비우고 커서 두기
   todoInput.value = "";
   todoInput.focus();
 });
 
-// 엔터 키 입력 시 클릭 이벤트 발생
+todoList.addEventListener("change", (event) => {
+  // 체크박스에서 이벤트 발생할 때
+  if (event.target && event.target.type === "checkbox") {
+    const checkboxId = event.target.id; // task1
+    console.log("id: ", checkboxId, "체크 여부: ", event.target.checked);
+
+    // task 부분을 지우고, 남은 부분을 가져온다.
+    const idNumber = checkboxId.replace("task", "");
+    console.log(typeof idNumber); // STRING
+
+    // 체크박스의 id 와 일치하는 todo 객체를 배열에서 가져온다.
+    const target = todos.find((todo) => todo.id === Number(idNumber));  
+
+    if (target) {
+      target.done = !target.done; // 체크 상태 업데이트
+
+      saveTodos(todos); // 로컬 스토리지에 저장
+      
+      renderTodos(todos); // UI 업데이트
+    }
+  }
+});
+
+// Enter 누를 때 클릭 이벤트 발생
 todoInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     todoBtn.click();
